264372b245cc024036410b987768c9a19da17ac9
Renato Xavier <59875607+renatoxdsr@users.noreply.github.com>
2026-05-30 14:43:17 +0100

    Create common-and-leap-year-check.py

---
+++ common-and-leap-year-check.py
@@ -0,0 +1,32 @@
+""" As you surely know, due to some astronomical reasons, years may be leap or common. The former are 366 days long, while the latter are 365 days long.
+
+Since the introduction of the Gregorian calendar (in 1582), the following rule is used to determine the kind of year:
+
+if the year number isn't divisible by four, it's a common year;
+otherwise, if the year number isn't divisible by 100, it's a leap year;
+otherwise, if the year number isn't divisible by 400, it's a common year;
+otherwise, it's a leap year.
+"""
+
+year = int(input("Enter a year: "))
+
+if year < 1582:
+	print("Not within the Gregorian calendar period")
+else:
+    #  Write the if-elif-elif-else block here.
+    # if the year number isn't divisible by four, it's a common year;
+    if year%4 != 0:
+        print('Common year')
+    elif year %100 != 0:
+        # otherwise, if the year number isn't divisible by 100, it's a leap year;
+        print('Leap year')
+    elif year %400 != 0:
+        # otherwise, if the year number isn't divisible by 400, it's a common year;
+        print('Common year')
+    else:
+        # otherwise, it's a leap year.
+        print('Leap year')
+    
+    
+    
+	
